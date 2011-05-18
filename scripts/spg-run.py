3e7167af08024deb8fb7da45ccc0894a811a60f2
Claudio J. Tessone <claudio.tessone@gmail.com>
2011-05-18 21:28:45 +0000

    (CT) updated code
    git-svn-id: http://web.sg.ethz.ch/tessonec-svn/devel/spg@1210 020870f9-e149-0410-bbc0-a08dbd5c884d

--- scripts/spg-run.py
+++ scripts/spg-run.py
@@ -1,12 +1,37 @@
 #!/usr/bin/python
 
 
-from spg import SPGParser
+import sqlite3 as sql
 
-  
-parser = SPGParser()
+from spg import Parser
+
+class Launcher(Parser):
+    def __init__(self, stream=None, db_name = "results.sqlite"):
+        Parser.__init__(self, stream)
+        self.connection =  sqlite3.connect(db_name)
+#        connection.text_factory = lambda x: unicode(x, "utf-8", "ignore")
+        self.cursor = self.connection.cursor()
+        self.__init_db()
+        
+    def __init_db(self):
+        cursor.execute("CREATE TABLE values ADD COLUMN groupid INTEGER") 
+        #cursor.execute("ALTER TABLE projects ADD COLUMN downloaded INTEGER")
+        #cursor.execute("ALTER TABLE projects ADD COLUMN error INTEGER") 
+        #connection.commit()
+
+
+
+
+
+
+
+
+
+
+
+parser = Launcher()
 
 parser.fetch( open("param.dat") )
 
 for i in parser:
-        print i
+    print i
