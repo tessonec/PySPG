de8722eedcdff6d6cdb55f2abe209e48ec746f38
Claudio J. Tessone <claudio.tessone@gmail.com>
2011-06-09 16:30:54 +0000

    (CT) spg-mngr has queue manipulation
    git-svn-id: http://web.sg.ethz.ch/tessonec-svn/devel/spg@1269 020870f9-e149-0410-bbc0-a08dbd5c884d

--- scripts/spg-mngr.py
+++ scripts/spg-mngr.py
@@ -26,143 +26,256 @@ def register_process():
         self.stdout_contents = params.contents_in_output(self.command)
 
 
-    def __init_db(self):
-
-        #:::~ Table with the name of the executable
-        self.cursor.execute( "SELECT name FROM executable " )
-        self.command = self.cursor.fetchone()[0]
-
-#        #:::~ Table with the constant values
-#        self.cursor.execute( "SELECT name,value FROM constants " )
-#        for k, v in self.cursor:
-#        #    self.constants[k] = v
-#            self.values[k] = v
-#            
-        
+#    def __init_db(self):
+#
+#        #:::~ Table with the name of the executable
+#        self.cursor.execute( "SELECT name FROM executable " )
+#        self.command = self.cursor.fetchone()[0]
+#
+##        #:::~ Table with the constant values
+##        self.cursor.execute( "SELECT name,value FROM constants " )
+##        for k, v in self.cursor:
+##        #    self.constants[k] = v
+##            self.values[k] = v
+##            
+#        
+##        #:::~ get the names of the columns
+##        self.cursor.execute("PRAGMA table_info(variables)")
+##        self.entities = [ i[1] for i in self.cursor.fetchall() ]
+##        self.entities = self.entities[1:]
+#
 #        #:::~ get the names of the columns
-#        self.cursor.execute("PRAGMA table_info(variables)")
-#        self.entities = [ i[1] for i in self.cursor.fetchall() ]
-#        self.entities = self.entities[1:]
+#        self.cursor.execute("SELECT name FROM entities ORDER BY id")
+#        self.entities = [ i[0] for i in self.cursor ]
+##        self.entities = self.entities[1:]
+#
+#
+#        #:::~ get the names of the outputs
+#        self.cursor.execute("PRAGMA table_info(results)")
+#        self.output_column = [ i[1] for i in self.cursor.fetchall() ]
+#        self.output_column = self.output_column[1:]
+#        
+##        print self.entities
+#        
+#    def __iter__(self):
+#        return self
+#
+#    def next(self):
+#        
+#        self.cursor.execute(
+#                    "SELECT r.id, r.values_set_id, %s FROM run_status AS r, values_set AS v "% ", ".join(["v.%s"%i for i in self.entities]) +
+#                    "WHERE r.status = 'N' AND v.id = r.values_set_id ORDER BY r.id LIMIT 1" 
+#                   )
+#                   
+#        res = self.cursor.fetchone()
+#        if res == None:
+#          raise StopIteration
+##        print res    
+##        res = list(res)
+#        self.current_run_id  = res[0]
+#        self.current_variables_id  = res[1]
+#        self.cursor.execute( 'UPDATE run_status SET status ="R" WHERE id = %d'%self.current_run_id  )
+#        self.connection.commit()          
+##        print res.keys(), dir(res)
+#        for i in range(len(self.entities)):
+#            self.values[ self.entities[i] ] = res[i+2]
+#        return self.values
+#
+#    def generate_tree(self, dir_vars = None):
+#        if dir_vars:
+#            self.directory_vars = dir_vars
+#        else:
+#            self.directory_vars =  self.entities 
+#
+#    def launch_process(self):
+#        pwd = os.path.abspath(".")
+#        if self.directory_vars:
+#            dir = utils.replace_list(self.directory_vars, self.values, separator = "/")
+#            if not os.path.exists(dir): os.makedirs(dir)
+#            os.chdir(dir)
+#        configuration_filename = "input_%d.dat"%self.current_run_id
+#        fconf = open(configuration_filename,"w")
+#        
+#        for k in self.values.keys():
+#            print >> fconf, k, utils.replace_string(self.values[k], self.values) 
+#        fconf.close()
+#        
+#        cmd = "%s/%s -i %s"%(BINARY_PATH, self.command, configuration_filename )
+#        print cmd
+#        proc = Popen(cmd, shell = True, stdin = PIPE, stdout = PIPE, stderr = PIPE )
+#        proc.wait()
+#        ret_code = proc.returncode
+##        ret_code = 0
+#        output = [i.strip() for i in proc.stdout.readline().split()]
+##        output = ["1" for i in self.output_column[1:] ]
+##        print ret_code, "-->", output
+#        os.remove(configuration_filename)
+#        if self.directory_vars:
+#            os.chdir(pwd)
+##        print self.output_column
+#        if ret_code == 0:
+#           self.cursor.execute( 'UPDATE run_status SET status ="D" WHERE id = %d'%self.current_run_id )
+#           all_d = [self.current_variables_id]
+#           all_d.extend( output )
+#           cc = 'INSERT INTO results ( %s) VALUES (%s) '%( ", ".join(self.output_column) , ", ".join([str(i) for i in all_d]) )
+#           print cc
+#           self.cursor.execute( cc )
+#           self.connection.commit()
+#        else:
+#           #:::~ status can be either 
+#           #:::~    'N': not run
+#           #:::~    'R': running
+#           #:::~    'D': successfully run (done)
+#           #:::~    'E': run but with non-zero error code
+#           self.cursor.execute( 'UPDATE run_status SET status ="E" WHERE id = %d'%self.current_run_id )
+#           self.connection.commit()
 
-        #:::~ get the names of the columns
-        self.cursor.execute("SELECT name FROM entities ORDER BY id")
-        self.entities = [ i[0] for i in self.cursor ]
-#        self.entities = self.entities[1:]
+######################################################################################################
+######################################################################################################
+######################################################################################################
+######################################################################################################
+######################################################################################################
+######################################################################################################
+######################################################################################################
+######################################################################################################
+######################################################################################################
+
+def get_parameters(arg):
+    ret = {}
+    
+    for i in arg.split(":"):
+        [k,v] = i.split("=")
+        ret[k] = v
+
+    return ret
+######################################################################################################
+######################################################################################################
+######################################################################################################
+######################################################################################################
+######################################################################################################
+######################################################################################################
 
 
-        #:::~ get the names of the outputs
-        self.cursor.execute("PRAGMA table_info(results)")
-        self.output_column = [ i[1] for i in self.cursor.fetchall() ]
-        self.output_column = self.output_column[1:]
+def process_queue(cmd, name, params):
+   # queue [add|remove|set|stop] QUEUE_NAME {params} 
+   
+ #                  "(id INTEGER PRIMARY KEY, name CHAR(64), max_jobs INTEGER, status CHAR(1))")
+ 
+   #checks whether the queue is in the 
+   cursor.execute( "SELECT id FROM queues WHERE name = '%s' "%name)
+   queue = cursor.fetchone()
+   if queue is not None:
+       (queue, ) = queue
+   
+   
+   if cmd == "add" and queue:
+          utils.newline_msg("SKP", "queue '%s' already exists"%name )
+          sys.exit(2)
+   elif cmd == "add" :
+       try:
+           max_jobs = params["jobs"]
+       except:
+           max_jobs = 1
+           
+       cursor.execute( "INSERT INTO queues (name, max_jobs, status) VALUES (?,?,?)",(name,  max_jobs, 'S') )
+   elif not queue:
+       utils.newline_msg("SKP", "queue does not '%s' exist"%name )
+       sys.exit(2)
+   elif cmd == "remove":
+       cursor.execute( "DELETE FROM queues WHERE id = ?",(queue,) )
+   elif cmd == "set":
+#       try:
+           max_jobs = params["jobs"]
+           cursor.execute( 'UPDATE queues SET max_jobs=? WHERE id = ?', ( max_jobs, queue ) )
+#       except:
+#           pass
+   elif cmd == "start":
+           cursor.execute( "UPDATE queues SET status = 'R' WHERE id = ?", (queue,) )
+   elif cmd == "stop":
+           cursor.execute( "UPDATE queues SET status = 'S' WHERE id = ?", (queue,) )
+   
+   else:
+       utils.newline_msg("SYN", "command '%s' not understood"%cmd )
+       sys.exit(1)
+   connection.commit()
+   
+   
+######################################################################################################
+
+dict_functions = { "register":register_process, "queue": process_queue }
+
+def execute_command( arguments ):
+    if len( arguments ) <3 :
+        return
         
-#        print self.entities
-        
-    def __iter__(self):
-        return self
+    full_command = arguments[0]
 
-    def next(self):
-        
-        self.cursor.execute(
-                    "SELECT r.id, r.values_set_id, %s FROM run_status AS r, values_set AS v "% ", ".join(["v.%s"%i for i in self.entities]) +
-                    "WHERE r.status = 'N' AND v.id = r.values_set_id ORDER BY r.id LIMIT 1" 
-                   )
-                   
-        res = self.cursor.fetchone()
-        if res == None:
-          raise StopIteration
-#        print res    
-#        res = list(res)
-        self.current_run_id  = res[0]
-        self.current_variables_id  = res[1]
-        self.cursor.execute( 'UPDATE run_status SET status ="R" WHERE id = %d'%self.current_run_id  )
-        self.connection.commit()          
-#        print res.keys(), dir(res)
-        for i in range(len(self.entities)):
-            self.values[ self.entities[i] ] = res[i+2]
-        return self.values
+    cmd = arguments[1] 
+    name = arguments[2]
+    if len(arguments) > 3 :
+        params = get_parameters( arguments[3] )
+    else:
+        params = {}
 
-    def generate_tree(self, dir_vars = None):
-        if dir_vars:
-            self.directory_vars = dir_vars
-        else:
-            self.directory_vars =  self.entities 
+    f = dict_functions[ full_command ] 
+    f(cmd, name, params)
 
-    def launch_process(self):
-        pwd = os.path.abspath(".")
-        if self.directory_vars:
-            dir = utils.replace_list(self.directory_vars, self.values, separator = "/")
-            if not os.path.exists(dir): os.makedirs(dir)
-            os.chdir(dir)
-        configuration_filename = "input_%d.dat"%self.current_run_id
-        fconf = open(configuration_filename,"w")
-        
-        for k in self.values.keys():
-            print >> fconf, k, utils.replace_string(self.values[k], self.values) 
-        fconf.close()
-        
-        cmd = "%s/%s -i %s"%(BINARY_PATH, self.command, configuration_filename )
-        print cmd
-        proc = Popen(cmd, shell = True, stdin = PIPE, stdout = PIPE, stderr = PIPE )
-        proc.wait()
-        ret_code = proc.returncode
-#        ret_code = 0
-        output = [i.strip() for i in proc.stdout.readline().split()]
-#        output = ["1" for i in self.output_column[1:] ]
-#        print ret_code, "-->", output
-        os.remove(configuration_filename)
-        if self.directory_vars:
-            os.chdir(pwd)
-#        print self.output_column
-        if ret_code == 0:
-           self.cursor.execute( 'UPDATE run_status SET status ="D" WHERE id = %d'%self.current_run_id )
-           all_d = [self.current_variables_id]
-           all_d.extend( output )
-           cc = 'INSERT INTO results ( %s) VALUES (%s) '%( ", ".join(self.output_column) , ", ".join([str(i) for i in all_d]) )
-           print cc
-           self.cursor.execute( cc )
-           self.connection.commit()
-        else:
-           #:::~ status can be either 
-           #:::~    'N': not run
-           #:::~    'R': running
-           #:::~    'D': successfully run (done)
-           #:::~    'E': run but with non-zero error code
-           self.cursor.execute( 'UPDATE run_status SET status ="E" WHERE id = %d'%self.current_run_id )
-           self.connection.commit()
 
 
 if __name__ == "__main__":
+    
+    ##################################################################################################
+    #### :::~ (begin) DB connection 
+    
+    connection = sql.connect("%s/running.sqlite"%VAR_PATH)
+    cursor = connection.cursor()
+    
+    #:::~ status can be either 
+    #:::~    'S': stopped
+    #:::~    'R': running
+    #:::~    'F': finished
+    cursor.execute("CREATE TABLE IF NOT EXISTS sets "
+                   "(id INTEGER PRIMARY KEY, full_name CHAR(256), path CHAR(256), db_name CHAR(256), status CHAR(1), "
+                   " total_combinations INTEGER, done_combinations INTEGER, running_combinations INTEGER, error_combinations INTEGER,  "
+                   " weight FLOAT)")
+    
+    #:::~ status can be either 
+    #:::~    'S': stopped
+    #:::~    'R': running
+    cursor.execute("CREATE TABLE IF NOT EXISTS queues "
+                   "(id INTEGER PRIMARY KEY, name CHAR(64), max_jobs INTEGER, status CHAR(1))")
+    
+    connection.commit()
+    
+    #### :::~ ( end ) DB connection 
+    ##################################################################################################
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
-    parser = optparse.OptionParser(usage = "usage: %prog [options] project_id1 project_id2 project_id3... ")
-    parser.add_option("--timeout", type="int", action='store', dest="timeout",
-                            default = 60 , help = "timeout for database connection" )
+    parser = optparse.OptionParser(usage = "usage: %prog [options] CMD {arguments}"
+                                      "commands: "
+                                      "   queue [add|remove|set|start|stop] QUEUE_NAME {params} "
+                                      "        params::: jobs=NJOBS"
+                                  )
 
-    parser.add_option("--tree", action='store_true', dest="tree",
-                       help = "whether to create a directory tree with the key-value pairs" )
-
-    parser.add_option("-d","--directory-var", action='store', type = "string", dest="directory_vars",
-                       default = False, help = "which variables to store as directories, only if tree" )
+#    parser.add_option("--tree", action='store_true', dest="tree",
+#                       help = "whether to create a directory tree with the key-value pairs" )
+#
+#    parser.add_option("-d","--directory-var", action='store', type = "string", dest="directory_vars",
+#                       default = False, help = "which variables to store as directories, only if tree" )
 
     options, args = parser.parse_args()
     
-    if len(args) == 0:
-        args = ["results.sqlite"]
+    execute_command(args)
     
-    for i_arg in args:
-      if ".sqlite" not in i_arg:
-          db_name = i_arg.replace("parameters","").replace(".dat","")
-          db_name = "results%s.sqlite"%db_name
-      else:
-          db_name = i_arg
-
-      executor = DBExecutor( db_name, timeout = options.timeout)
-      
-      if options.tree:
-          executor.generate_tree( options.directory_vars )
-
-      for values in executor:
-          executor.launch_process()
-          
-          #      parser.init_db()
-#          parser.fill_status(repeat = options.repeat )
-
+    
+    
