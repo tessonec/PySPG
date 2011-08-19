33814af845283103efb669daf64023e9c4639d42
Claudio J. Tessone <claudio.tessone@gmail.com>
2011-08-19 21:08:43 +0000

    git-svn-id: http://web.sg.ethz.ch/tessonec-svn/devel/spg@1563 020870f9-e149-0410-bbc0-a08dbd5c884d

--- scripts/spg-db.py
+++ scripts/spg-db.py
@@ -363,8 +363,6 @@ class DBCommandParser(cmd.Cmd):
 #        return True
     
 
-
-
 if __name__ == '__main__':
     cmd_line = DBCommandParser()
     if len(sys.argv) == 1:
@@ -386,45 +384,3 @@ if __name__ == '__main__':
 
 
 
-
-
-
-
-
-
-###
-###
-###
-###
-###import cmd
-###
-###class HelloWorld(cmd.Cmd):
-###    """Simple command processor example."""
-###    
-###    FRIENDS = [ 'Alice', 'Adam', 'Barbara', 'Bob' ]
-###    
-###    def do_greet(self, person):
-###        "Greet the person"
-###        if person and person in self.FRIENDS:
-###            greeting = 'hi, %s!' % person
-###        elif person:
-###            greeting = "hello, " + person
-###        else:
-###            greeting = 'hello'
-###        print greeting
-###    
-###    def complete_greet(self, text, line, begidx, endidx):
-###        if not text:
-###            completions = self.FRIENDS[:]
-###        else:
-###            completions = [ f
-###                            for f in self.FRIENDS
-###                            if f.startswith(text)
-###                            ]
-###        return completions
-###
-###    def do_EOF(self, line):
-###        return True
-###
-###if __name__ == '__main__':
-###    HelloWorld().cmdloop()
