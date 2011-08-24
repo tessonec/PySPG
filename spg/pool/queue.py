08119e10a36074433ac2aea52d84a69e1eedab75
Claudio J. Tessone <claudio.tessone@gmail.com>
2011-08-24 09:00:14 +0000

    (CT) updated code
    git-svn-id: http://web.sg.ethz.ch/tessonec-svn/devel/spg@1581 020870f9-e149-0410-bbc0-a08dbd5c884d

--- spg/pool/queue.py
+++ spg/pool/queue.py
@@ -5,21 +5,19 @@ from spg import BINARY_PATH, VAR_PATH
 
 
 class Queue:
-    def __init__(self, name, max_jobs):
+    def __init__(self, name, max_workers):
         self.name = name
-        self.jobs = max_jobs
-        self.waiting_processes = 100
-        self.processes = []
-        self.last_finished_processes = 0
+        self.max_workers = max_workers
+        self.workers= []
 
 
     def normalise_workers(self):
-        running_proc = len( self.processes )
+        running_proc = len( self.workers)
 #        print running_proc
-        if running_proc > self.jobs :
-            self.kill_workers( running_proc - self.jobs )
-        elif running_proc < self.jobs :
-            self.spawn_workers( self.jobs - running_proc  )
+        if running_proc > self.max_workers :
+            self.kill_workers( running_proc - self.max_workers )
+        elif running_proc < self.max_workers :
+            self.spawn_workers( self.max_workers - running_proc  )
 
     def spawn_workers( self, new_jobs ):
         """How many processes to populate"""
@@ -27,31 +25,31 @@ class Queue:
             cmd = ["%s/spg-worker.py"%BINARY_PATH, "--queue=%s"%self.name]
 #            proc = Popen(cmd, shell = True, stdin = PIPE, stdout = PIPE, stderr = PIPE )
             proc = Popen(cmd, stdin = PIPE, stdout = PIPE, stderr = PIPE )
-            self.processes.append(proc)
+            self.workers.append(proc)
 #            proc.wait()
 
 
     def kill_workers(self, n_jobs = None):
         tbr = []
         if not n_jobs: 
-            for proc in self.processes:
+            for proc in self.workers:
                 proc.kill() 
                 proc.wait()
                 tbr.append(proc)
         else:
-            for proc in self.processes[:n_jobs] :
+            for proc in self.workers[:n_jobs] :
                 proc.kill() 
                 proc.wait()
                 tbr.append(proc)
         for proc in tbr:
-            self.processes.remove(proc)
+            self.workers.remove(proc)
 
 #            self.master_db.execute("DELETE FROM running WHERE job_id = ?" , i)
 
     def update_worker_info(self):  # These are the spg-worker instances in the queueing system
-        tbr = [ p for p in self.processes if p.poll() is not None]
+        tbr = [ p for p in self.workers if p.poll() is not None]
         print tbr
         for proc in tbr:
-            self.processes.remove(proc)
+            self.workers.remove(proc)
 
 
--- spg/pool/torque.py
+++ spg/pool/torque.py
@@ -8,19 +8,25 @@ from queue import Queue
 class TorqueQueue(Queue):
 
     def spawn_workers( self, new_jobs ):
+        print "spawning workers: ", new_jobs
         for i in range(new_jobs):
             cmd = "qsub -q %s %s/spg-worker.py --queue=%s"%(self.name, BINARY_PATH, self.name)
             proc = Popen(cmd, shell = True, stdin = PIPE, stdout = PIPE, stderr = PIPE )
-            self.processes.append(proc)
-#            proc.wait()
+#            self.workers.append(proc)
+            proc.wait()
 
     def kill_workers( self, n_jobs = None):
+        print "killing workers: ", n_jobs
         if not n_jobs: 
-            n_jobs = len(self.processes)
-        for i in sorted(self.processes)[:n_jobs] :
-            cmd = "qdel %s"%(i)
-            proc = Popen(cmd, shell = True, stdin = PIPE, stdout = PIPE, stderr = PIPE )
-            proc.wait()
+            for i in self.workers:
+                cmd = "qdel %s"%(i)
+                proc = Popen(cmd, shell = True, stdin = PIPE, stdout = PIPE, stderr = PIPE )
+                proc.wait()
+        else:
+            for i in self.workers[:n_jobs]:
+                cmd = "qdel %s"%(i)
+                proc = Popen(cmd, shell = True, stdin = PIPE, stdout = PIPE, stderr = PIPE )
+                proc.wait()
 #            self.master_db.execute("DELETE FROM running WHERE job_id = ?" , i)
 
     def update_worker_info(self):  # These are the spg-worker instances in the queueing system
@@ -28,15 +34,16 @@ class TorqueQueue(Queue):
         output = proc.stdout
         proc.wait()
         # 9640.bruetli spg-worker.py tessonec 22:53:44 R default  
-        self.processes = []
+        self.workers = []
         for l in output:
             content = l.split()
             try:
                 job_id = int( content[0].split(".")[0] )
 #                status = content[4]
                 if  self.name ==  content[5]:
-                    self.processes.append( job_id ) 
+                    self.workers.append( job_id ) 
             except: 
                 continue
 
+        print "number of workers: ", len(self.workers)
 
