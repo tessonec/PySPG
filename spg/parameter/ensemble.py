e302877c1aaed78f914590da01503fb798b9bca3
Claudio J. Tessone <claudio.tessone@gmail.com>
2011-09-04 11:06:49 +0000

    git-svn-id: http://web.sg.ethz.ch/tessonec-svn/devel/spg@1649 020870f9-e149-0410-bbc0-a08dbd5c884d

--- spg/parameter/ensemble.py
+++ spg/parameter/ensemble.py
@@ -288,7 +288,7 @@ class ParameterEnsembleExecutor(ParameterEnsemble):
             dir = utils.generate_string(self.values,self.directory_vars, joining_string = "/")
             if not os.path.exists(dir): os.makedirs(dir)
             os.chdir(dir)
-        configuration_filename = "input_%s_%d.dat"%(self.current_run_id)
+        configuration_filename = "input_%.8d.dat"%(self.current_run_id)
         fconf = open(configuration_filename,"w")
         
         for k in self.values.keys():
@@ -337,7 +337,7 @@ class ParameterEnsembleInputFilesGenerator(ParameterEnsemble):
    #         dir = utils.generate_string(self.values,self.directory_vars, joining_string = "/")
    #         if not os.path.exists(dir): os.makedirs(dir)
     #        os.chdir(dir)
-        configuration_filename = "input_%%8.d.dat"%(self.db_name, self.current_valuesset_id)
+        configuration_filename = "input_%8.d.dat"%(self.current_valuesset_id)
         fconf = open(configuration_filename,"w")
         
         for k in self.values.keys():
