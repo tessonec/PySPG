6ab84db14daaa0f8bcadbcf239c1cfcd0bb36340
Claudio J. Tessone <claudio.tessone@gmail.com>
2011-09-23 16:32:50 +0000

    (CT)
    git-svn-id: http://web.sg.ethz.ch/tessonec-svn/devel/spg@1673 020870f9-e149-0410-bbc0-a08dbd5c884d

--- spg/parameter/ensemble.py
+++ spg/parameter/ensemble.py
@@ -284,6 +284,7 @@ class ParameterEnsembleExecutor(ParameterEnsemble):
     def __init__(self, full_name = "", id=-1, weight=1., queue = '*', status = 'R', repeat = 1, init_db = True):
         ParameterEnsemble.__init__(self, full_name , id, weight, queue , status , repeat  , init_db )
         self.generate_tree()
+        self.directory_vars = self.variables[:]
         os.chdir(self.path)
            
     def launch_process(self):
