8d45f9fe3c9089b5672f8dc848bd86361f80e10c
Claudio J. Tessone <claudio.tessone@gmail.com>
2011-12-17 19:40:43 +0100

    added killing non producing workers

--- scripts/spg-master.py
+++ scripts/spg-master.py
@@ -100,6 +100,7 @@ if __name__ == "__main__":
             harvests_without_results = 0
         
         if options.kill_workers_after and harvests_without_results > options.kill_workers_after :
+             inline_msg("WRN", "killing workers", indent = 2)
              for (name, max_jobs) in ls_queues:
                 all_queues[name].kill_workers()
             
