63e449d0c2129598de4b2816f05257c674f7ce86
Claudio Tessone <claudio.tessone@uzh.ch>
2017-06-07 09:21:16 +0200

    CT running processes always proportional to weight

--- spg/utils/tools.py
+++ spg/utils/tools.py
@@ -6,6 +6,7 @@ from math import *
 
 
 def str_color(s):
+    "simple colour implementation"
     color_dict = {"red": "\033[31m","green": "\033[32m","yellow": "\033[33m","blue": "\033[34m","magenta": "\033[35m","cyan": "\033[36m", "reset":'\033[0m'}
 
 
